e3bddf213159539240d836679b09cb98fb752acb
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-05 17:01:22 +0500

    feat: implement create text file in files folder

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { writeFile } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+    const fileName = 'fresh.txt';
+    const modulePath = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(modulePath, 'files', fileName)
+
+    const content = 'I am fresh and young';
+
+    try {
+        await writeFile(filePath, content, { flag: 'ax'});
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
