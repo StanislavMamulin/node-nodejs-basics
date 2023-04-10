08d5168f705f5b64d2108fd2cb1576169ebb4540
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-10 21:27:45 +0500

    feat: implement read file using streams api

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    const filename = './files/fileToRead.txt';
+    const fileURL = new URL(filename, import.meta.url);
+    const fileStream = createReadStream(fileURL);
+
+    let content = '';
+
+    fileStream.on('data', chunk => { content += chunk });
+    fileStream.on('error', error => { console.error('Error', error.message)});
+    fileStream.on('end', () => { console.log(content) });
 };
 
 await read();
