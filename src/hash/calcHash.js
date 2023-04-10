1470d1124e4fe5abb10570cb344e77a92fcbce6d
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-10 20:58:27 +0500

    feat: implement calculation SHA256 hash for file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createReadStream } from 'node:fs';
+const { createHash } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256'); 
+
+    const filename = './files/fileToCalculateHashFor.txt';
+    const fileURL = new URL(filename, import.meta.url);
+
+    const fileStream = createReadStream(fileURL);
+    fileStream.on('readable', () => {
+        const data = fileStream.read();
+        if (data) {
+            hash.update(data);
+        } else {
+            console.log(hash.digest('hex'));
+        }
+    })
 };
 
 await calculateHash();
