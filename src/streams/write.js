61d3c9507ab5ae2ccbc637357ad3bb2b63b0c6fc
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-10 22:01:35 +0500

    feat: implement write data into file

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,32 @@
+import { createWriteStream } from 'node:fs';
+import { createInterface } from 'node:readline';
+
 const write = async () => {
-    // Write your code here 
+    const filename = './files/fileToWrite.txt';
+    const fileURL = new URL(filename, import.meta.url);
+    const fileStream = createWriteStream(fileURL);
+    
+    const greeting = 'Enter text to write to the file.\
+    \nTo finish writing, please type "exit" or press "ctrl + c"';
+
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    console.log(greeting);
+    rl.on('line', line => {
+        if (line.toLowerCase() === 'exit') {
+            rl.close();
+            return;
+        }
+        
+        fileStream.write(line += '\n');
+    });
+  
+    rl.on('close', () => {
+        console.log('That\'s all Folks!!!');
+    });
 };
 
 await write();
