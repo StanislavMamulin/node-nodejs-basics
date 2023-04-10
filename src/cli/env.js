0729f69779e7e552ce1b90e953abf7471aa6c69d
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-10 15:00:35 +0500

    feat: implement print rss env variables to console

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rssKeys = Object.entries(process.env).filter(([key, _]) => key.includes('RSS'));
+    const rssKeysString = rssKeys.map((entry) => entry.join('=')).join('; ');
+    console.log(rssKeysString);
 };
 
 parseEnv();
