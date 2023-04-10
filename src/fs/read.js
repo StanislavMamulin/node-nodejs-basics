37d6df7a495194d59a733e9390f0d343887c43f8
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-10 14:11:05 +0500

    feat: implement print content of file into console

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import { stat, readFile } from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    const ERROR_TEXT = 'FS operation failed';
+    const fileName = 'files/fileToRead.txt';
+
+    const fileURL = new URL(fileName, import.meta.url);
+    
+    try {
+        const dstFileExist = await stat(fileURL).then(_ => true).catch(_ => false);
+        if (!dstFileExist) {
+            throw new Error(ERROR_TEXT);
+        }
+
+        const fileContent = await readFile(fileURL, { encoding: 'utf-8' });
+        console.log(fileContent);
+    } catch(err) {
+        throw new Error(err);
+    }
 };
 
 await read();
