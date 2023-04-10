b2352fb44bbbe16ddd3e6e46eb3b99ce777daf6b
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-10 13:49:44 +0500

    feat: implement delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import path from 'node:path';
+import promises from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+
 const remove = async () => {
-    // Write your code here 
+    const ERROR_TEXT = 'FS operation failed';
+    const fileName = 'files/fileToRemove.txt';
+
+    const modulePath = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(modulePath, fileName);
+    
+    try {
+        const dstFileExist = await promises.stat(filePath).then(_ => true).catch(_ => false);
+        if (!dstFileExist) {
+            throw new Error(ERROR_TEXT);
+        }
+
+        await promises.rm(filePath)
+    } catch(err) {
+        throw new Error(ERROR_TEXT);
+    }
 };
 
 await remove();
