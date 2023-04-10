420f255df6ad68c92f6ebfe6397fa695207b33b3
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-10 23:47:15 +0500

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
+const numWorkers = cpus().length;
+
+function runWorker(workerData) {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker('./src/wt/worker.js', { workerData });
+        worker.on('message', (result) => resolve({ status: 'resolved', data: result }));
+        worker.on('error', (error) => reject({ status: error, data: null }));
+        worker.on('exit', (code) => {
+            if (code !== 0)
+                reject(new Error(`Stopped the Worker Thread with the exit code: ${code}`));
+        })
+    })
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const promises = [];
+    let dataForWorker = 10;
+    for (let i = 0; i < numWorkers; i++) {
+        promises.push(runWorker(dataForWorker++));
+    }
+
+    const results = await Promise.all(promises);
+    console.log(results);
 };
 
 await performCalculations();
